e68ed629042ad3f749d30b15fd4dd79b9504c033
zeejee94 <jzz0226@gmail.com>
2020-03-11 01:45:12 +0800

    ok
    ok

---
+++ src/firebase/init.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyA7TrcWuwT_aA47eWV6CDusXgYfrEt5eUU",
+  authDomain: "fpy-dev.firebaseapp.com",
+  databaseURL: "https://fpy-dev.firebaseio.com",
+  projectId: "fpy-dev",
+  storageBucket: "fpy-dev.appspot.com",
+  messagingSenderId: "371598464406"
+  };
+firebase.initializeApp(config);
+const databaseRef = firebase.database().ref();
+export const userRef = databaseRef.child("users");
+export const authRef = firebase.auth();
