c97a76a0e26c4619a697a1348f6627d27508307f
zeejee94 <jzz0226@gmail.com>
2020-04-13 21:31:04 +0800

    VR can be done

--- src/views/Pages/index.js
+++ src/views/Pages/index.js
@@ -2,7 +2,7 @@ import Login from './Login';
 import Page404 from './Page404';
 import Page500 from './Page500';
 import Register from './Register';
-
+import Vr from './Vr';
 export {
-  Login, Page404, Page500, Register
+  Login, Page404, Page500, Register,Vr
 };
---
+++ src/views/VR/Vr.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Vr.css';
+
+class Vr extends React.Component {
+    render() {
+        return (
+            <div className="Vr">
+                <a-scene arjs='sourceType: webcam; debugUIEnabled: false'>
+                    <a-anchor hit-testing-enabled="true">
+                        <a-entity position='0 0 0' scale='0.05 0.05 0.05' rotation='200 0 0' obj-model='obj: url(models/dog.obj); mtl: url(models/dog.mtl)'></a-entity>
+                        {/*<a-box position='0 0 0.5' material='opacity: 0.7; color: yellow;'></a-box>*/}
+                    </a-anchor>
+                    <a-camera-static preset="hiro" />
+                </a-scene>
+            </div>
+        );
+    }
+}
+
+export default Vr;
