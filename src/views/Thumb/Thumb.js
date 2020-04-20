abb6007c8fb3e6587397a03ccf4b98ee13a81827
zeejee94 <jzz0226@gmail.com>
2020-04-21 03:48:27 +0800

    revert

--- src/views/Thumb/Thumb.js
+++ src/views/Thumb/Thumb.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Zoom from 'react-medium-image-zoom';
-import 'react-medium-image-zoom/dist/styles.css';
+
 
 const Thumb = props => (
   <div className={props.classes}>
-    <Zoom>
     <img src={props.src} alt={props.alt} title={props.title} />
-    </Zoom>
   </div>
 );
 
