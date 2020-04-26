4dd3d5fd4d728bd8ce7746f831cc0b2e0f9ec86a
zeejee94 <jzz0226@gmail.com>
2020-04-26 13:53:29 +0800

    map

--- src/views/Product/productUpload.js
+++ src/views/Product/productUpload.js
@@ -6,6 +6,13 @@ import { userRef, authRef } from '../../firebase/init';
 import { addUser, editUser} from '../../actions/usersactions';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { classnames } from './helpers';
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng,
+  } from 'react-places-autocomplete';
+
+  
 class productUpload extends Component {
   constructor(props) {
     super(props)
@@ -24,7 +31,11 @@ setIntialData = () => {
         key: 0,
         picture : null,
         pictureUrl : null,
-
+        address: '',
+        errorMessage: '',
+        latitude: null,
+        longitude: null,
+        isGeocoding: false,
     }
 }
 // componentDidMount() {
@@ -84,9 +95,59 @@ setIntialData = () => {
 //   reader.readAsDataURL(file);
 
 // }
+handleChange = address => {
+    this.setState({
+      address,
+      latitude: null,
+      longitude: null,
+      errorMessage: '',
+    });
+  };
+
+  handleSelect = selected => {
+    this.setState({ isGeocoding: true, address: selected });
+    geocodeByAddress(selected)
+      .then(res => getLatLng(res[0]))
+      .then(({ lat, lng }) => {
+        this.setState({
+          latitude: lat,
+          longitude: lng,
+          isGeocoding: false,
+        });
+      })
+      .catch(error => {
+        this.setState({ isGeocoding: false });
+        console.log('error', error); // eslint-disable-line no-console
+      });
+  };
+
+  handleCloseClick = () => {
+    this.setState({
+      address: '',
+      latitude: null,
+      longitude: null,
+    });
+  };
+  handleError = (status, clearSuggestions) => {
+    console.log('Error from Google Maps API', status); // eslint-disable-line no-console
+    this.setState({ errorMessage: status }, () => {
+      clearSuggestions();
+    });
+  };
+
+
 render() {
+    const {
+        address,
+        errorMessage,
+        latitude,
+        longitude,
+        isGeocoding,
+      } = this.state;
     return (
         <div className="animated fadeIn">
+         
+     
             <Row>
                 <Col xs="12" md="6">
                     <Card>
@@ -142,7 +203,88 @@ render() {
                                         <Label htmlFor="password-input">User Role</Label>
                                     </Col>
                                     <Col xs="12" md="9">
-                                        <Input type="text" name="select" id="userrole" value={this.state.userrole} readOnly = {true}/>
+                                    <PlacesAutocomplete
+          onChange={this.handleChange}
+          value={address}
+          onSelect={this.handleSelect}
+          onError={this.handleError}
+          shouldFetchSuggestions={address.length > 2}
+        >
+          {({ getInputProps, suggestions, getSuggestionItemProps }) => {
+            return (
+              <div className="Demo__search-bar-container">
+                <div className="Demo__search-input-container">
+                  <input
+                    {...getInputProps({
+                      placeholder: 'Search Places...',
+                      className: 'Demo__search-input',
+                    })}
+                  />
+                  {this.state.address.length > 0 && (
+                    <button
+                      className="Demo__clear-button"
+                      onClick={this.handleCloseClick}
+                    >
+                      x
+                    </button>
+                  )}
+                </div>
+                {suggestions.length > 0 && (
+                  <div className="Demo__autocomplete-container">
+                    {suggestions.map(suggestion => {
+                      const className = classnames('Demo__suggestion-item', {
+                        'Demo__suggestion-item--active': suggestion.active,
+                      });
+
+                      return (
+                        /* eslint-disable react/jsx-key */
+                        <div
+                          {...getSuggestionItemProps(suggestion, { className })}
+                        >
+                          <strong>
+                            {suggestion.formattedSuggestion.mainText}
+                          </strong>{' '}
+                          <small>
+                            {suggestion.formattedSuggestion.secondaryText}
+                          </small>
+                        </div>
+                      );
+                      /* eslint-enable react/jsx-key */
+                    })}
+                    <div className="Demo__dropdown-footer">
+                      
+                    </div>
+                  </div>
+                )}
+              </div>
+            );
+          }}
+        </PlacesAutocomplete>
+        {errorMessage.length > 0 && (
+          <div className="Demo__error-message">{this.state.errorMessage}</div>
+        )}
+
+        {((latitude && longitude) || isGeocoding) && (
+          <div>
+            <h3 className="Demo__geocode-result-header">Geocode result</h3>
+            {isGeocoding ? (
+              <div>
+                <i className="fa fa-spinner fa-pulse fa-3x fa-fw Demo__spinner" />
+              </div>
+            ) : (
+              <div>
+                <div className="Demo__geocode-result-item--lat">
+                  <label>Latitude:</label>
+                  <span>{latitude}</span>
+                </div>
+                <div className="Demo__geocode-result-item--lng">
+                  <label>Longitude:</label>
+                  <span>{longitude}</span>
+                </div>
+              </div>
+            )}
+          </div>
+        )}
                                             
                                        
                                         <FormText className="help-block">Please enter your phone</FormText>
